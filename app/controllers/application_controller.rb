fa2479d0305803c0e5274c790fd4b2dbb52a204a
tatsuki199 <tanukityomari@gmail.com>
2024-06-22 10:39:17 +0900

    Summary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenicate_or_request_with_http_basic do |username, password|
+    authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
     end
   end
